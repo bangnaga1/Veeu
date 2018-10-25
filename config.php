f6a27001159099592ec913d1ffcb4465d22abf53
bangnaga1 <bangnaga183@gmail.com>
2018-10-26 01:33:38 +0700

    Version 0.1

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+//masukan config token yg terdapat pada link di packet captur
+// jangan hapus tanda kutip(" ") 
+
+$access_token = "0d8ebd3a-2feb-4401-9e3d-bbcfbb5cafc9";
+
+// kamu bisa membuat banyak config untuk beberapa akun.
